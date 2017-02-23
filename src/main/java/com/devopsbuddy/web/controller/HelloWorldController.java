8f3cbda10c211895837b0032cc5bb9f1e59bc1b1
Gergely Madacsi <madgergely@hotmail.com>
2017-02-23 20:20:01 +0100

    HelloWorldController has been moved

--- src/main/java/com/devopsbuddy/web/controller/HelloWorldController.java
+++ src/main/java/com/devopsbuddy/web/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.devopsbuddy.controller;
+package com.devopsbuddy.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
